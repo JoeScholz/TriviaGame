0d8910ef2c6541b3f394b43c24742aee97ade0fc
Joey2Jobs <scholz.joe@gmail.com>
2018-12-19 13:29:44 -0600

    questions and ?timer?

---
+++ assets/javascript/app.js
@@ -0,0 +1,162 @@
+// var windowTimeout = setTimeout(function() {
+//     alert("Are you ready for trivia? Timer starts immediately...")
+//   }, 1000);
+var startScreen;
+var scoreOverScreen;
+var timeOutScreen;
+
+var timer;
+var timeCount = :60;
+
+var correctAns = 0;
+var wrongAns = 0;
+var missed = 0;
+
+var timeInterval;
+
+function startTimer (
+  timeInterval = setInterval(resetTimer, 1000);
+)
+
+  function start() {
+    $("#countDown").html("5:00");
+  }
+  
+start();
+  var questions = [
+  {  
+  question: "1. Question1?",
+  answers: [
+    "answer1a",
+    "answer2a"
+    "answer3a"
+    "answer4a"
+  ],
+  correctAnswer: "answer1a",
+  name: "topic1"
+  }
+
+  {  
+    question: "1. Question1?",
+    answers: [
+      "answer1a",
+      "answer2a"
+      "answer3a"
+      "answer4a"
+    ],
+    correctAnswer: "answer1a",
+    name: "topic1"
+    }
+
+    {  
+      question: "1. Question1?",
+      answers: [
+        "answer1a",
+        "answer2a"
+        "answer3a"
+        "answer4a"
+      ],
+      correctAnswer: "answer1a",
+      name: "topic1"
+      }
+
+      {  
+        question: "2. Question2?",
+        answers: [
+          "answer1b",
+          "answer2b"
+          "answer3b"
+          "answer4b"
+        ],
+        correctAnswer: "answer1b",
+        name: "topic2"
+        }
+
+        {  
+          question: "3. Question3?",
+          answers: [
+            "answer1c",
+            "answer2c"
+            "answer3c"
+            "answer4c"
+          ],
+          correctAnswer: "answer1c",
+          name: "topic3"
+          }
+
+          {  
+            question: "4. Question4?",
+            answers: [
+              "answer1d",
+              "answer2d"
+              "answer3d"
+              "answer4d"
+            ],
+            correctAnswer: "answer1d",
+            name: "topic4"
+            }
+
+            {  
+              question: "5. Question5?",
+              answers: [
+                "answer1e",
+                "answer2e"
+                "answer3e"
+                "answer4e"
+              ],
+              correctAnswer: "answer1e",
+              name: "topic5"
+              }
+
+              {  
+                question: "6. Question6?",
+                answers: [
+                  "answer1f",
+                  "answer2f"
+                  "answer3f"
+                  "answer4f"
+                ],
+                correctAnswer: "answer1f",
+                name: "topic6"
+                }
+
+                {  
+                  question: "7. Question7?",
+                  answers: [
+                    "answer1g",
+                    "answer2g"
+                    "answer3g"
+                    "answer4g"
+                  ],
+                  correctAnswer: "answer1g",
+                  name: "topic7"
+                  }
+                
+                  {  
+                    question: "8. Question8?",
+                    answers: [
+                      "answer1h",
+                      "answer2h"
+                      "answer3h"
+                      "answer4h"
+                    ],
+                    correctAnswer: "answer1h",
+                    name: "topic8"
+                    }
+]
+// make timer
+  //set to decrease by seconds
+    //reset after time out or game over
+
+//make questions
+  //make answers
+    //set to store score if true or false
+
+//make start screen
+  //starts countdown and first question
+
+//make final screen
+  //shows score when game finished or time up
+
+//make reset
+  //
